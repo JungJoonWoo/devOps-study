b3880ba03d90803720abc7c2e4cfea99cd09be5f
JungJoonWoo <jjw2030@ajou.ac.kr>
2024-04-07 23:51:08 +0900

    front

---
+++ web-application/day22/image-click-fn.js
@@ -0,0 +1,19 @@
+
+// var sum = 0;
+// for (let i = 0; i < 10; i++) {
+//   sum += i;
+// }
+// alert("1부터 10까지의 합은: " + sum);
+
+function over() {
+  let elementById = document.getElementById("img");
+  // obj.src = "../images/book1.jpg"
+  elementById.src="../images/book1.jpg";
+}
+
+function out() {
+  let elementById = document.getElementById("img");
+  // obj.src = "../images/book2.jpg";
+  elementById.src="../images/book2.jpg";
+
+}
